6e2867595b32103ec764bf3add38d1085a53ff67
Павел Михайлов <pasha8165@gmail.com>
2024-09-29 14:14:07 +0300

    ability now automatically fill up on html Also they are read only for now hardcoded user value for while waiting until models are done

--- charlist/views.py
+++ charlist/views.py
@@ -2,11 +2,25 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# Hard coded af, waiting for xdmav
+# Hard coded af, waiting for xdmav's models
+user_value = {
+    'abilities' :{
+        'Strength': 12,
+        'Constitution' : 14,
+        'Dexterity' : 13,
+        'Intelligence' : 15,
+        'Wisdom' : 6,
+        'Charisma' : 17,
+    }
+}
+
+
 class CharListStats(TemplateView):
     template_name = "charlist/CharListStats.html"
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["abilities"] = ["Strength","Constitution","Dexterity","Intelligence","Wisdom","Charisma"]
+
         context["senses"] = ["Insight","Perception"]
+        context['user_value']=user_value
         return context
