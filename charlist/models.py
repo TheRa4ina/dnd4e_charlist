9c18953026bcb764aa689240b23121f8d90579f0
TheRa4ina <pasha8165@gmail.com>
2024-09-30 16:57:43 +0300

    temporal unfull
    make populate migration after
    
    still unfinished but onw crazy
    
    Finally finished db and population

--- charlist/models.py
+++ charlist/models.py
@@ -2,21 +2,25 @@ from django.db import models
 from django.conf import settings
 
 class Game(models.Model):
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Game_Users")
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Game_User")
     name = models.CharField(max_length=200)
 
-class Game_Users(models.Model):
+class Game_User(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-class Game_GMs(models.Model):
+class Game_GM(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     gm = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Gender(models.Model):
+    def __str__(self) -> str:
+        return self.gender
     gender = models.CharField(max_length=50,primary_key=True)
 
 class Size(models.Model):
+    def __str__(self) -> str:
+        return self.size
     size = models.CharField(max_length=50,primary_key=True)
 
 class Character(models.Model):
@@ -25,16 +29,21 @@ class Character(models.Model):
     char_class = models.CharField(max_length=200, null=True)
     race = models.CharField(max_length=200, null=True)
     xp = models.IntegerField(default=0)
-    # SIZES = {
-    #     "T": "Tiny",
-    #     "S": "Small",
-    #     "M": "Medium",
-    #     "L": "Large",
-    #     "H": "Huge",
-    #     "G": "Gargantuan",
-    # }
-    size = models.ForeignKey(Size)
-    gender = models.ForeignKey(Gender)
+    SIZES = {
+        "T": "Tiny",
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+        "H": "Huge",
+        "G": "Gargantuan",
+    }
+    size = models.CharField(max_length=1, choices=SIZES, default="M")
+    GENDERS = {
+        "M": "Male",
+        "F": "Female",
+        "O": "Other",
+    }
+    gender = models.CharField(max_length=1, choices=GENDERS, default="M")
     height = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
     alignment = models.CharField(max_length=200, null=True)
@@ -49,21 +58,27 @@ class Character_Health:
     hp = models.IntegerField(default=0)
     temp_hp = models.IntegerField(default=0)
     surges_day = models.IntegerField(default=0)
+    surges_left = models.IntegerField(default=0)
+    surges_bonus = models.IntegerField(default=0)
 
-class Abilities(models.Model):
+class Ability(models.Model):
+    def __str__(self) -> str:
+        return self.ability
     ability = models.CharField(max_length=200,primary_key=True)
 
-class Skills(models.Model):
+class Skill(models.Model):
+    def __str__(self) -> str:
+        return self.skill
     skill = models.CharField(max_length=200,primary_key=True)
 
 class Character_Ability(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    ability = models.ForeignKey(Abilities,on_delete=models.CASCADE)
+    ability = models.ForeignKey(Ability,on_delete=models.CASCADE)
     score = models.IntegerField(default=10)
 
-class Character_Skills(models.Model):
+class Character_Skill(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skills,on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
     score=models.IntegerField(default=10)
     trained=models.BooleanField(default=False)
 
@@ -81,11 +96,20 @@ class Character_Initiative_Extra(models.Model):
     extra_name = models.CharField(max_length=50)
     score=models.IntegerField(default=0)
 
-class Character_Trained_Skills(models.Model):
+class Character_Skill_Extra(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skills, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+    score=models.IntegerField(default=0)
 
+class Character_Trained_Skill(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    
 class Character_Notes(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     note_name=models.CharField(max_length=100)
+    note_name=models.CharField(max_length=100)
     note = models.CharField(max_length=2000)
+
+
--- charlist/views.py
+++ charlist/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import *
 
 # Hard coded af, waiting for xdmav's models
 user_value = {
@@ -18,17 +19,14 @@ class CharListStats(TemplateView):
     template_name = "charlist/CharListStats.html"
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["abilities"] = ["strength","constitution","dexterity","intelligence","wisdom","charisma"]
-        context["skills"] = ["acrobatic","arcana","athletic","bluff","diplomacy",
-                             "dungeoning","endurange","heal","history","insight",
-                             "intimidate","nature","perception","religion","stealth",
-                             "streetwise","thievery"]
+        context["abilities"] = Ability.objects.all()
+        context["skills"] = Skill.objects.all()
         # Сюда бы еще добавить на чем эти скилы депендент, но мне впадлу
         context["senses"] = ["insight","perception"]
         context['user_value']=user_value
         return context
 
-# Also hardcoded af, still waiting for models
+# Still hardcoded asf
 user_value["sessions"] = ["eberron", "test_sesh", "dm_testing"]
 
 class SessionSelection(TemplateView):
