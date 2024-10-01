64b7aa394aeb06f29784491fb6784cf0eb9cd3c6
TheRa4ina <pasha8165@gmail.com>
2024-10-01 17:47:40 +0300

    still unfinished but onw crazy

--- charlist/models.py
+++ charlist/models.py
@@ -13,27 +13,28 @@ class Game_GMs(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     gm = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+class Gender(models.Model):
+    gender = models.CharField(max_length=50,primary_key=True)
+
+class Size(models.Model):
+    size = models.CharField(max_length=50,primary_key=True)
+
 class Character(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     char_class = models.CharField(max_length=200, null=True)
     race = models.CharField(max_length=200, null=True)
     xp = models.IntegerField(default=0)
-    SIZES = {
-        "T": "Tiny",
-        "S": "Small",
-        "M": "Medium",
-        "L": "Large",
-        "H": "Huge",
-        "G": "Gargantuan",
-    }
-    size = models.CharField(max_length=1, choices=SIZES, default="M")
-    GENDERS = {
-        "M": "Male",
-        "F": "Female",
-        "O": "Other",
-    }
-    gender = models.CharField(max_length=1, choices=GENDERS, default="M")
+    # SIZES = {
+    #     "T": "Tiny",
+    #     "S": "Small",
+    #     "M": "Medium",
+    #     "L": "Large",
+    #     "H": "Huge",
+    #     "G": "Gargantuan",
+    # }
+    size = models.ForeignKey(Size)
+    gender = models.ForeignKey(Gender)
     height = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
     alignment = models.CharField(max_length=200, null=True)
@@ -50,50 +51,41 @@ class Character_Health:
     surges_day = models.IntegerField(default=0)
 
 class Abilities(models.Model):
-    ability = models.CharField(max_length=200)
+    ability = models.CharField(max_length=200,primary_key=True)
 
 class Skills(models.Model):
-    skill = models.CharField(max_length=200)
+    skill = models.CharField(max_length=200,primary_key=True)
 
-class Character_Ability:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
-    strength = models.IntegerField(default=0)
-    constitution = models.IntegerField(default=0)
-    dexterity = models.IntegerField(default=0)
-    intelligence = models.IntegerField(default=0)
-    wisdom = models.IntegerField(default=0)
-    charisma = models.IntegerField(default=0)
+class Character_Ability(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    ability = models.ForeignKey(Abilities,on_delete=models.CASCADE)
+    score = models.IntegerField(default=10)
 
-class Character_Skills:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
-    arcana    = models.IntegerField(default=0)
-    athletic  = models.IntegerField(default=0)
-    bluff     = models.IntegerField(default=0)
-    diplomacy = models.IntegerField(default=0)
-    dungeoning= models.IntegerField(default=0)
-    endurange = models.IntegerField(default=0)
-    heal      = models.IntegerField(default=0)
-    history   = models.IntegerField(default=0)
-    insight   = models.IntegerField(default=0)
-    intimidate= models.IntegerField(default=0)
-    nature    = models.IntegerField(default=0)
-    perception= models.IntegerField(default=0)
-    religion  = models.IntegerField(default=0)
-    stealth   = models.IntegerField(default=0)
-    streetwise= models.IntegerField(default=0)
-    thievery  = models.IntegerField(default=0)
+class Character_Skills(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skills,on_delete=models.CASCADE)
+    score=models.IntegerField(default=10)
+    trained=models.BooleanField(default=False)
 
-class Character_Defenses:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
+
+class Character_Defenses_Extra(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
     armor_coefficient = models.IntegerField(default=0)
     fortres = models.IntegerField(default=0)
     reflex = models.IntegerField(default=0)
     will = models.IntegerField(default=0)
 
-class Character_Trained_Skills:
+class Character_Initiative_Extra(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
+    score=models.IntegerField(default=0)
+
+class Character_Trained_Skills(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     skill = models.ForeignKey(Skills, on_delete=models.CASCADE)
 
-class Character_Notes:
+class Character_Notes(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    note_name=models.CharField(max_length=100)
     note = models.CharField(max_length=2000)
