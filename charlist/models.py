abd4788ce6b3ac1b2d47f9e8c94feb9447b5de20
TheRa4ina <pasha8165@gmail.com>
2024-10-04 12:42:28 +0300

    Models, views use models

--- charlist/models.py
+++ charlist/models.py
@@ -2,16 +2,26 @@ from django.db import models
 from django.conf import settings
 
 class Game(models.Model):
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Game_Users")
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Game_User")
     name = models.CharField(max_length=200)
 
-class Game_Users(models.Model):
+class Game_User(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-class Game_GMs(models.Model):
+class Game_GM(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     gm = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Gender(models.Model):
+    def __str__(self) -> str:
+        return self.gender
+    gender = models.CharField(max_length=50,primary_key=True)
+
+class Size(models.Model):
+    def __str__(self) -> str:
+        return self.size
+    size = models.CharField(max_length=50,primary_key=True)
 
 class Character(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
@@ -19,21 +29,8 @@ class Character(models.Model):
     char_class = models.CharField(max_length=200, null=True)
     race = models.CharField(max_length=200, null=True)
     xp = models.IntegerField(default=0)
-    SIZES = {
-        "T": "Tiny",
-        "S": "Small",
-        "M": "Medium",
-        "L": "Large",
-        "H": "Huge",
-        "G": "Gargantuan",
-    }
-    size = models.CharField(max_length=1, choices=SIZES, default="M")
-    GENDERS = {
-        "M": "Male",
-        "F": "Female",
-        "O": "Other",
-    }
-    gender = models.CharField(max_length=1, choices=GENDERS, default="M")
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender,on_delete=models.CASCADE)
     height = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
     alignment = models.CharField(max_length=200, null=True)
@@ -48,52 +45,60 @@ class Character_Health:
     hp = models.IntegerField(default=0)
     temp_hp = models.IntegerField(default=0)
     surges_day = models.IntegerField(default=0)
+    surges_left = models.IntegerField(default=0)
+    surges_bonus = models.IntegerField(default=0)
 
-class Abilities(models.Model):
-    ability = models.CharField(max_length=200)
+class Ability(models.Model):
+    def __str__(self) -> str:
+        return self.ability
+    ability = models.CharField(max_length=200,primary_key=True)
 
-class Skills(models.Model):
-    skill = models.CharField(max_length=200)
+class Skill(models.Model):
+    def __str__(self) -> str:
+        return self.skill
+    skill = models.CharField(max_length=200,primary_key=True)
 
-class Character_Ability:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
-    strength = models.IntegerField(default=0)
-    constitution = models.IntegerField(default=0)
-    dexterity = models.IntegerField(default=0)
-    intelligence = models.IntegerField(default=0)
-    wisdom = models.IntegerField(default=0)
-    charisma = models.IntegerField(default=0)
+class Character_Ability(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    ability = models.ForeignKey(Ability,on_delete=models.CASCADE)
+    score = models.IntegerField(default=10)
 
-class Character_Skills:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
-    arcana = models.IntegerField(default=0)
-    athletic = models.IntegerField(default=0)
-    bluff = models.IntegerField(default=0)
-    diplomacy = models.IntegerField(default=0)
-    dungeoning = models.IntegerField(default=0)
-    endurange = models.IntegerField(default=0)
-    heal = models.IntegerField(default=0)
-    history = models.IntegerField(default=0)
-    insight = models.IntegerField(default=0)
-    intimidate = models.IntegerField(default=0)
-    nature = models.IntegerField(default=0)
-    perception = models.IntegerField(default=0)
-    religion = models.IntegerField(default=0)
-    stealth = models.IntegerField(default=0)
-    streetwise = models.IntegerField(default=0)
-    thievery = models.IntegerField(default=0)
+class Character_Skill(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+    score=models.IntegerField(default=10)
+    trained=models.BooleanField(default=False)
 
-class Character_Defenses:
-    character = models.OneToOneField(Character, on_delete=models.CASCADE)
+
+class Character_Defenses_Extra(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
     armor_coefficient = models.IntegerField(default=0)
     fortres = models.IntegerField(default=0)
     reflex = models.IntegerField(default=0)
     will = models.IntegerField(default=0)
 
-class Character_Trained_Skills:
+class Character_Initiative_Extra(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skills, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
+    score=models.IntegerField(default=0)
 
-class Character_Notes:
+class Character_Skill_Extra(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    extra_name = models.CharField(max_length=50)
+    skill = models.ForeignKey(Skill,on_delete=models.CASCADE)
+    score=models.IntegerField(default=0)
+
+class Character_Trained_Skill(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+class Character_Notes(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    note_name=models.CharField(max_length=100)
     note = models.CharField(max_length=2000)
+
+class Gm_Notes(models.Model):
+    gm = models.ForeignKey(Game_GM, on_delete=models.CASCADE)
+    note_name = models.CharField(max_length=100)
+    note = models.TextField()
