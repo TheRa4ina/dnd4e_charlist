107d5cfda4a39cbdd4791008fe75c04689681512
Павел Михайлов <pasha8165@gmail.com>
2024-10-06 17:42:07 +0300

    preparation for multiple chars per user

--- charlist/urls.py
+++ charlist/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 app_name = "charlist"
 urlpatterns = [
-    path("<slug:session_name>/stats/", views.CharListStats.as_view(), name="CharListStats"),
+    path("<slug:session_name>/<slug:char_name>/stats/", views.CharListStats.as_view(), name="CharListStats"),
     path("",views.SessionSelection.as_view(),name="SessionSelecion")
 ]
