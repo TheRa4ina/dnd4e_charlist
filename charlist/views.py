a0fd764742ba9533c0367b9e7cefd7311ea3b33e
Павел Михайлов <pasha8165@gmail.com>
2024-10-07 22:55:32 +0300

    placeholders for selection

--- charlist/views.py
+++ charlist/views.py
@@ -1,7 +1,9 @@
 from typing import Any
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect,HttpRequest
 from django.views.generic.base import TemplateView
 from .models import *
+from django.urls import reverse
 
 # Hard coded af, waiting for xdmav's models
 user_value = {
@@ -22,12 +24,29 @@ class CharListStats(TemplateView):
         context["basis"] = ["name","class","race","size","age",
                             "gender","height","weight","algnment",
                             "deity","max-hp","surges-day","level"]
-        context["abilities"] = Ability.objects.all()
+        context["abilities"] = Ability.objects.values_list("ability",flat=True)#flat, so it returns single values
         context["skills"] = Skill.objects.all()
         context["senses"] = ["insight","perception"]
         context["defenses"] = ["armor_coefficient","fortitude","reflex","will"]
         context['user_value']=user_value
         return context
+    
+
+class CharCreator(TemplateView):
+    template_name="charlist/CharCreator.html"
+
+def add_char(request,session_name):
+    try:
+        char_name = request.POST["name"]
+    except(KeyError):
+        return HttpResponse("Invalid post")
+    else:
+        #char-creation-placeholder
+
+        return HttpResponseRedirect(reverse("charlist:CharListStats",kwargs={
+            "session_name":session_name,
+        }))
+
 
 # Still hardcoded asf
 user_value["sessions"] = ["eberron", "test_sesh", "dm_testing"]
@@ -38,3 +57,15 @@ class SessionSelection(TemplateView):
         context = super().get_context_data(**kwargs)
         context['user_value']=user_value
         return context
+
+class SessionCreator(TemplateView):
+    template_name="charlist/SessionCreator.html"
+
+def add_session(request: HttpRequest):
+    try:
+        session_name = request.POST["name"]
+    except(KeyError):
+        return HttpResponse("Invalid post")
+    else:
+        #session-creation-placeholder
+        return HttpResponseRedirect(reverse("charlist:SessionSelector"))
