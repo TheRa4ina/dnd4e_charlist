85eb546ee774f8f6ff84740c014ed75c39ef1447
TheRa4ina <pasha8165@gmail.com>
2024-10-08 12:45:42 +0300

    urls cleanup

--- charlist/urls.py
+++ charlist/urls.py
@@ -5,8 +5,8 @@ app_name = "charlist"
 urlpatterns = [
     path("<slug:session_name>/stats/", views.CharListStats.as_view(), name="CharListStats"),
     path("",views.SessionSelection.as_view(),name="SessionSelector"),
-    path("<slug:session_name>/add_char/",views.add_char,name="AddChar"),
     path("<slug:session_name>/char_creator/",views.CharCreator.as_view(),name="CharCreator"),
+    path("<slug:session_name>/add_char/",views.add_char,name="AddChar"),
     path("session_creator/",views.SessionCreator.as_view(),name="SessionCreator"),
     path("create_session/",views.add_session,name="AddSession"),
 ]
