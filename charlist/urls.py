018eba5f2f4e2e89a04679bcc48465076ca505ee
TheRa4ina <pasha8165@gmail.com>
2024-10-10 13:26:01 +0300

    urls cleanup

--- charlist/urls.py
+++ charlist/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 app_name = "charlist"
 urlpatterns = [
-    path("<slug:session_name>/stats/", views.CharListStats.as_view(), name="CharListStats"),
+    path("session_creator/",views.SessionCreator.as_view(),name="SessionCreator"),
+    path("create_session/",views.add_session,name="AddSession"),
     path("",views.SessionSelection.as_view(),name="SessionSelector"),
     path("<slug:session_name>/char_creator/",views.CharCreator.as_view(),name="CharCreator"),
     path("<slug:session_name>/add_char/",views.add_char,name="AddChar"),
-    path("session_creator/",views.SessionCreator.as_view(),name="SessionCreator"),
-    path("create_session/",views.add_session,name="AddSession"),
+    path("<slug:session_name>/<slug:char_name>/stats/", views.CharListStats.as_view(), name="CharListStats"),
 ]
