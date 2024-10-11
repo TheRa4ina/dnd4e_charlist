6c183777c40f4d3b5dadc0908c425d7af747fe3f
TheRa4ina <pasha8165@gmail.com>
2024-10-11 12:37:37 +0300

    Invitation url implemented. invite adds to session_user

--- charlist/views.py
+++ charlist/views.py
@@ -2,6 +2,7 @@ from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect,HttpRequest
 from django.views.generic.base import TemplateView
+from django.core import exceptions
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 from django.urls import reverse
@@ -33,6 +34,25 @@ def add_session(request: HttpRequest):
         Session_GM.objects.create(session=new_session,gm=current_user)
         Session_Invitation.objects.create(session = new_session)
         return HttpResponseRedirect(reverse("charlist:SessionSelector"))
+    
+def join_session(request: HttpRequest,invitation_key):
+    try:
+        session = Session.objects.get(session_invitation__key = invitation_key)
+    except(Session_Invitation.DoesNotExist):
+        return HttpResponse("Invalid invitation key")
+    current_user = request.user
+    user_is_gm = Session_GM.objects.filter(Q(gm = current_user) & Q(session=session)).exists()
+    user_is_player = Session_User.objects.filter(Q(user = current_user) & Q(session = session)).exists()
+    user_already_in = user_is_gm | user_is_player
+
+    if(user_already_in):
+        return HttpResponse("You are already in this session")
+    else:
+        Session_User.objects.create(session=session,user=current_user)
+        return HttpResponseRedirect(reverse("charlist:CharSelector",kwargs={
+            "session_id" : session.id,
+            }))
+    
 
 class SessionSelection(LoginRequiredMixin,TemplateView):
     login_url="admin/"
@@ -44,6 +64,8 @@ class SessionSelection(LoginRequiredMixin,TemplateView):
             Q(session_gm__gm = cur_user) | Q(session_user__user = cur_user)
             )
         return context
+    
+
     
 class CharSelector(TemplateView):
     template_name= "charlist/CharSelector.html"
